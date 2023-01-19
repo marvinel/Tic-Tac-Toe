119c6b62a98ce03a134bc6bb2673c7b6896e64fd
Marvin Henriquez <mhenriquezb@olimpica.com.co>
2023-01-19 09:51:48 -0500

    Turns done

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+import './App.css'
+
+const TURNS = {
+  X: 'X',
+  O: 'O'
+}
+
+
+const Square = ({children,isSelected, updateBoard, index})=>{
+const className = `square ${isSelected ? 'is-selected':''}`
+const handleClick = () =>{
+  updateBoard()
+} 
+
+  return (
+    <div className={className} onClick={handleClick}>
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  const [board, setState] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = () =>{
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+  return (
+    <main className="board">
+      <h1> Tic Tac Toe</h1>
+      <section className='game'> 
+      {
+        board.map((__, index) =>{
+          return (
+            <Square 
+              key={index} 
+              index= {index}
+              updateBoard={updateBoard}  
+            >
+                 {board[index]}
+            </Square>
+          )
+
+        })
+      }
+      </section>
+      <section className='turn'>
+        <Square isSelected={ turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={ turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+    </main>
+  )
+}
+
+export default App

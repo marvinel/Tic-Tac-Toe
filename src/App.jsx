ba83e4de82467bcc7d7040004c9448afe40ef4c5
Marvin Henriquez <mhenriquezb@olimpica.com.co>
2023-01-19 16:52:11 -0500

    game done

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,16 @@ import { TURNS } from './constant'
 import { checkWinner, checkEndGame } from './logic/board'
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(()=>{
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+    
+  })
+  const [turn, setTurn] = useState(()=>{
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ? turnFromStorage :  TURNS.X
+
+  })
   const [winner, setWinner] = useState(null)
 
  
@@ -18,7 +26,12 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
+
+  
   const updateBoard = (index) =>{
 
     if(board[index] || winner) return
@@ -29,6 +42,10 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    //Guardar partida
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
     
    const newWinner = checkWinner(newBoard)
     
